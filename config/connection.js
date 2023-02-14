effadf1c29563946a9df4d5157cd438e757e928d
Jayden Griffith <griffithjayden97@outlook.com>
2023-02-14 00:34:20 -0700

    Continued testing

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,19 @@ require('dotenv').config();
 
 let sequelize;
 
-sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: `0.0.0.0`,
-    dialect: 'mysql',
-    port: process.env.PORT
-  }
-);
+if (process.env.MYSQL_URL) {
+  sequelize = new Sequelize(process.env.MYSQL_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.PORT,
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
