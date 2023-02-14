23fb10f02c49d5dca9661c99d0bf8909febdec01
Jayden Griffith <griffithjayden97@outlook.com>
2023-02-14 00:36:09 -0700

    Trial and error

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.MYSQL_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.PORT,
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
